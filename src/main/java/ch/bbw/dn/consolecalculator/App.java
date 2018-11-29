bc7b505e5780bb65f3920f8f99715647456e07c5
Dominik <naef.dominik@hotmail.com>
2018-11-29 21:40:01 +0100

    Implement addition into Application Class.

--- src/main/java/ch/bbw/dn/consolecalculator/App.java
+++ src/main/java/ch/bbw/dn/consolecalculator/App.java
@@ -1,13 +1,27 @@
 package ch.bbw.dn.consolecalculator;
 
 /**
- * Hello world!
+ * Console Calculator App
+ * @author Dominik Näf
+ * @version 0.0.1
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    
+	public static void main( String[] args ){
+		
+		Calculator calculator = new Calculator();
+		int valueA = 0;
+		int valueB = 0;
+		
+		System.out.println("Colsole Calculator");
+		System.out.println("==================");
+		System.out.println();
+		
+		valueA = 10;
+		valueA = 20;
+		System.out.println("Summe " + valueA + " + " + valueB + " = " + calculator.summe(10, 20));
+		
     }
 }
