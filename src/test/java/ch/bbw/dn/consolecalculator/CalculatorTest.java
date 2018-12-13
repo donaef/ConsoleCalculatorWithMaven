1711bea3957b517a34ea4743b254563544d69c2c
Dominik <naef.dominik@hotmail.com>
2018-12-14 00:18:43 +0100

    Implement division with min_value in CalculatorTest class.

--- src/test/java/ch/bbw/dn/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/dn/consolecalculator/CalculatorTest.java
@@ -132,6 +132,11 @@ public class CalculatorTest {
 		assertTrue(testee.division(Integer.MAX_VALUE, 2) == Integer.MAX_VALUE / 2);
 	}
 	
+	@Test
+	public void testDivisionEineMinValueEinePositivIsOk() {
+		assertTrue(testee.division(Integer.MIN_VALUE, 2) == Integer.MIN_VALUE / 2);
+	}
+	
 	//protected - Der Testfall muss sich im gleichen Paket wie die getestete Methode befinden.
 
 	//private 	- Das Testen von privaten Methoden ist etwas komplizierter. Es gibt die Möglichkeit "dp4j" und eine "Reflection" zu verwenden.
